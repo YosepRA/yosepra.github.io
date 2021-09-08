28f8eac45baa9afcdd081037685048d5b5533190
YosepRA <yosep.ryan.yra@gmail.com>
2021-09-08 15:36:36 +0700

    🎨 Add contact form validation and sending state.

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Row, Col, Form } from 'react-bootstrap';
-import { Formik, Field } from 'formik';
+import { Row, Col, Form, Spinner } from 'react-bootstrap';
+import { Formik, Field, ErrorMessage } from 'formik';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cn from 'classnames';
+import * as yup from 'yup';
 
 import mainStyles from '../styles/main.module.scss';
 import contactStyles from '../styles/contact.module.scss';
@@ -19,6 +20,14 @@ const initialValues = {
   subject: '',
   message: '',
 };
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Please enter a valid email.')
+    .required('This field is required.'),
+  subject: yup.string().required('This field is required.'),
+  message: yup.string().required('This field is required.'),
+});
 
 function Contacts() {
   const handleSend = async (values, { resetForm }) => {
@@ -82,36 +91,63 @@ function Contacts() {
               </Col>
 
               <Col md={8} lg={6} className="contact-form">
-                <Formik initialValues={initialValues} onSubmit={handleSend}>
-                  {({ handleSubmit }) => (
+                <Formik
+                  initialValues={initialValues}
+                  onSubmit={handleSend}
+                  validationSchema={schema}
+                >
+                  {({ handleSubmit, isSubmitting, errors, touched }) => (
                     <Form noValidate onSubmit={handleSubmit}>
                       <Form.Group controlId="email">
                         <Form.Label>Email address</Form.Label>
+
                         <Field name="email">
                           {({ field }) => (
                             <Form.Control
                               {...field}
                               type="email"
                               placeholder="your_email@mail.com"
+                              isInvalid={touched.email && !!errors.email}
+                              disabled={isSubmitting}
                             />
                           )}
                         </Field>
+
+                        <ErrorMessage name="email">
+                          {(msg) => (
+                            <Form.Control.Feedback type="invalid">
+                              {msg}
+                            </Form.Control.Feedback>
+                          )}
+                        </ErrorMessage>
                       </Form.Group>
 
                       <Form.Group controlId="subject">
                         <Form.Label>Subject</Form.Label>
+
                         <Field name="subject">
                           {({ field }) => (
                             <Form.Control
                               {...field}
                               placeholder="Message title"
+                              isInvalid={touched.subject && !!errors.subject}
+                              disabled={isSubmitting}
                             />
                           )}
                         </Field>
+
+                        <ErrorMessage name="subject">
+                          {(msg) => (
+                            <Form.Control.Feedback type="invalid">
+                              {msg}
+                            </Form.Control.Feedback>
+                          )}
+                        </ErrorMessage>
                       </Form.Group>
 
                       <Form.Group controlId="message">
                         <Form.Label>Message</Form.Label>
+
                         <Field name="message">
                           {({ field }) => (
                             <Form.Control
@@ -122,16 +158,43 @@ function Contacts() {
                                 contactStyles.contactMessageInput,
                                 contactStyles.formControlOverride,
                               )}
+                              isInvalid={touched.message && !!errors.message}
+                              disabled={isSubmitting}
                             />
                           )}
                         </Field>
+
+                        <ErrorMessage name="message">
+                          {(msg) => (
+                            <Form.Control.Feedback type="invalid">
+                              {msg}
+                            </Form.Control.Feedback>
+                          )}
+                        </ErrorMessage>
                       </Form.Group>
 
                       <button
                         className={contactStyles.contactSendBtn}
                         type="submit"
+                        disabled={isSubmitting}
                       >
-                        Send
+                        {isSubmitting ? (
+                          <>
+                            <Spinner
+                              animation="border"
+                              role="status"
+                              className={cn(
+                                contactStyles.contactSendBtnSpinner,
+                                contactStyles.spinnerBorderOverride,
+                              )}
+                            >
+                              <span className="sr-only">Sending...</span>
+                            </Spinner>{' '}
+                            Sending
+                          </>
+                        ) : (
+                          'Send'
+                        )}
                       </button>
                     </Form>
                   )}
